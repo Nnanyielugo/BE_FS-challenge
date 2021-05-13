20c25996c07b264ef52e886829d1265f58458832
Nnanyielugo <kz.onyegbule@gmail.com>
2021-05-13 10:51:42 +0100

    add source files

---
+++ src/interfaces.ts
@@ -0,0 +1,25 @@
+type Severity = 'temporary' | 'permanent';
+
+interface EventData {
+  event: string;
+  severity?: Severity;
+  timestamp: number;
+  id: string;
+}
+
+export interface Signature {
+  timestamp: string;
+  token: string;
+  signature: string;
+}
+
+export interface MailGunPostObject {
+  signature: Signature;
+  'event-data': EventData;
+}
+
+export interface SNSMessageBody {
+  Provider: string;
+  timestamp: number;
+  type: string;
+}
